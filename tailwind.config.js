86ad6dee8d236d9aa635ae4a44e6d3c970a3d761
Jorge Cruz <j.alexcruz@gmail.com>
2024-07-23 09:24:17 -0300

    set primary colors, add logo.png, screen in comments (02, 02.1)

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary':'#E7C236',
+        'secondary':'#F80606'
+      }
+    },
   },
   plugins: [],
 }
