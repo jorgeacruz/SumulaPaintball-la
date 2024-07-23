6147f3964bb5d36481cd1a36600f612c8ef7a51a
Jorge Cruz <j.alexcruz@gmail.com>
2024-07-23 07:38:53 -0300

    Install React + TailwindCss

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+   <div className='w-full h-screen flex flex-col items-center justify-center bg-black'>
+      <div>
+
+      <h1 className='text-white text-7xl'>Here</h1>
+      <h1 className='text-white text-2xl'>Here</h1>
+      </div>
+   </div>
+  );
+}
+
+export default App;
