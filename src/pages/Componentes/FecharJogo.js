c409398056427a58c956d5c324b32a56a425096d
Jorge Cruz <j.alexcruz@gmail.com>
2024-10-09 21:30:48 -0300

    componente Fechar Jogo

---
+++ src/pages/Componentes/FecharJogo.js
@@ -0,0 +1,7 @@
+export default function FecharPartida() {
+    return (
+      <button className="rounded-md w-[300px] h-[40px] bg-primary flex justify-center items-center">
+       <p>Fechar Partida</p>
+      </button>
+    );
+   }
