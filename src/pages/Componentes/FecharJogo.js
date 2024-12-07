66e31e5747e94068f99128849757d124389ae239
Gabriel Félix <felixgabriel.sant@gmail.com>
2024-12-06 23:57:09 -0300

    Delete src/pages/Componentes/FecharJogo.js

--- src/pages/Componentes/FecharJogo.js
+++
@@ -1,7 +0,0 @@
-export default function FecharPartida() {
-    return (
-      <button className="rounded-md w-[300px] h-[40px] bg-primary flex justify-center items-center">
-       <p>Fechar Partida</p>
-      </button>
-    );
-   }
